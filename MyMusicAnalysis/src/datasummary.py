fc03f60d59e2065ac07545410dab123ffa47f00a
sukumar0919 <144853332+sukumar0919@users.noreply.github.com>
2023-12-06 23:43:13 -0500

    Update datasummary.py

--- MyMusicAnalysis/src/datasummary.py
+++ MyMusicAnalysis/src/datasummary.py
@@ -3,24 +3,38 @@
 # Import necessary libraries
 import pandas as pd
 
-# Read the dataset
-dataset_path = "../Acoustic Features.csv"  # Adjust the path based on your actual structure
-data = pd.read_csv(dataset_path)
+def display_data_summary(dataset_path):
+    """
+    Display a summary of the dataset, including basic information, additional details, and attributes' data types.
 
-# Display basic information about the dataset
-print("Data Summary:")
-print(data.info())
+    Parameters:
+    - dataset_path (str): Path to the dataset CSV file.
 
-# Additional details about the dataset structure
-# (Modify this based on the actual structure of your dataset)
-num_use_cases = len(data)
-num_attributes = len(data.columns)
-attributes_data_types = {column: data[column].dtype for column in data.columns}
+    Returns:
+    - None
+    """
+    # Read the dataset
+    data = pd.read_csv(dataset_path)
 
-print("\nAdditional Details:")
-print(f"Number of Use Cases: {num_use_cases}")
-print(f"Number of Attributes: {num_attributes}")
+    # Display basic information about the dataset
+    print("Data Summary:")
+    print(data.info())
 
-print("\nAttributes and Data Types:")
-for attribute, data_type in attributes_data_types.items():
-    print(f"- {attribute}: {data_type}")
+    # Additional details about the dataset structure
+    # (Modify this based on the actual structure of your dataset)
+    num_use_cases = len(data)
+    num_attributes = len(data.columns)
+    attributes_data_types = {column: data[column].dtype for column in data.columns}
+
+    print("\nAdditional Details:")
+    print(f"Number of Use Cases: {num_use_cases}")
+    print(f"Number of Attributes: {num_attributes}")
+
+    print("\nAttributes and Data Types:")
+    for attribute, data_type in attributes_data_types.items():
+        print(f"- {attribute}: {data_type}")
+
+# Example Usage
+if __name__ == "__main__":
+    dataset_path = "../Acoustic Features.csv"  # Adjust the path based on your actual structure
+    display_data_summary(dataset_path)
